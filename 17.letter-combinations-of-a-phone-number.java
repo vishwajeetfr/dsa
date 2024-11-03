cba39fb09bcfac79f50bdeb8f4b0616f31693780
Vishwajeet Patil <113547757+vishwajeetfr@users.noreply.github.com>
2024-11-03 16:23:37 +0530

    First commit

---
+++ 17.letter-combinations-of-a-phone-number.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=17 lang=java
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Solution {
+
+    public List<String> letterCombinations(String digits)
+    {
+        if(digits.length()==0) return new ArrayList<String>();
+        HashMap<Character, ArrayList<Character>> digitMap = new HashMap<>();
+        digitMap.put('2',new ArrayList<>(Arrays.asList('a','b','c')));
+        digitMap.put('3',new ArrayList<>(Arrays.asList('d','e','f')));
+        digitMap.put('4',new ArrayList<>(Arrays.asList('g','h','i')));
+        digitMap.put('5',new ArrayList<>(Arrays.asList('j','k','l')));
+        digitMap.put('6',new ArrayList<>(Arrays.asList('m','n','o')));
+        digitMap.put('7',new ArrayList<>(Arrays.asList('p','q','r','s')));
+        digitMap.put('8',new ArrayList<>(Arrays.asList('t','u','v')));
+        digitMap.put('9',new ArrayList<>(Arrays.asList('w','x','y','z')));
+
+        return getLetterCombinations(digitMap,digits, 0, "",new ArrayList<String>());
+
+    }
+
+    public ArrayList<String> getLetterCombinations(HashMap<Character, ArrayList<Character>> digitMap,String digits, int index, String comb,ArrayList<String> sol)
+    {
+        if(index == digits.length())
+        {
+            sol.add(comb);
+            return sol;
+        }
+        
+       Character current =  digits.charAt(index);
+       ArrayList<Character> charList = digitMap.get(current);
+
+       for(char c : charList)
+       {
+           getLetterCombinations(digitMap,digits, index+1, comb+c,sol);
+           
+       }
+
+       return sol;
+    }
+}
+// @lc code=end
+
---
+++ 22.generate-parentheses.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=22 lang=java
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) 
+    {
+       return parentheses(1,0,n,"(",new ArrayList<String>());
+    }
+
+    public static List<String> parentheses(int open, int close,int n,String str, ArrayList<String> list)
+    {
+        if (open > n || close > n) {
+            return list;
+        }
+        if(close == n && open == n)
+        {
+            list.add(str);
+            return list;
+        }
+        parentheses(open+1, close , n, str+"(", list);
+        if(open > close) {
+            return parentheses(open, close + 1, n, str + ")", list);
+        }
+        return list;
+    }
+}
+
