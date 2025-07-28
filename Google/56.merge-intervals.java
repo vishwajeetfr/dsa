8b5fb0e1a728a0124d58259d88c71b0e2f651a41
Vishwajeet Patil <113547757+vishwajeetfr@users.noreply.github.com>
2025-07-28 15:04:48 +0530

    Back to pushing my code here after a break

---
+++ Google/56.merge-intervals.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=56 lang=java
+ *
+ * [56] Merge Intervals
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) 
+    {
+        int[][] sol = new int[1][1];
+        
+        return sol;
+    }
+}
+// @lc code=end
+
